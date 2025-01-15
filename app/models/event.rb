88c4ba973eb8b078a85b67c67c97540e27d98f1f
Matheus Santana <matheusfsantana99@gmail.com>
2025-01-15 16:25:57 -0300

    add mock and styles to events
    Co-authored-by: Lucas Soares Caetano <caetanolucassoares@gmail.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,7 +14,26 @@ class Event
   end
 
   def self.all
-    result = {}
+    result = [ Event.new(id: 1,
+                        name: "Event1",
+                        url: "",
+                        description: "Event1 description",
+                        start_date: "14-01-2025",
+                        end_date: "16-01-2025",
+                        event_type: "in-person",
+                        location: "Palhoça",
+                        participant_limit: 20,
+                        status: "published"),
+                    Event.new(id: 2,
+                        name: "Event2",
+                        url: "",
+                        description: "Event2 description",
+                        start_date: "15-01-2025",
+                        end_date: "17-01-2025",
+                        event_type: "in-person",
+                        location: "Florianópolis",
+                        participant_limit: 20,
+                        status: "draft") ]
     begin
       response = Faraday.get("localhost:3001/api/events")
 
