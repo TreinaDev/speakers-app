ce780673c227d0e192835771482e9e6dfe0c3ef8
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2025-01-21 18:25:23 -0300

    fix: bug with add task without contents
    Co-authored-by: Pedro Dias <PedroD98@users.noreply.github.com>

--- app/controllers/event_tasks_controller.rb
+++ app/controllers/event_tasks_controller.rb
@@ -27,6 +27,7 @@ class EventTasksController < ApplicationController
   end
 
   def check_event_content_owner
+    return unless current_user.event_contents.present?
     contents = params[:event_task][:event_content_ids].reject(&:blank?)
     contents.each do |content|
       unless current_user.event_contents.find_by(id: content)
