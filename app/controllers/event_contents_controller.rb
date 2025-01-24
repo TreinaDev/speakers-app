24b273c849cc86cff9ed957efa3119b8b97340e3
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2025-01-24 18:53:46 -0300

    feat: Usuário remove arquivos anexados aos conteúdos
    Co-authored-by: Pedro Dias <pedroddias98@gmail.com>

--- app/controllers/event_contents_controller.rb
+++ app/controllers/event_contents_controller.rb
@@ -1,13 +1,14 @@
 class EventContentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_event_content, only: %i[ show edit ]
+  before_action :set_event_content, only: %i[ show edit update ]
+  before_action :set_event_content_files, only: %i[ edit update ]
 
   def index
     @event_contents = current_user.event_contents
   end
 
-  def show
-  end
+  def show; end
+
   def new
     @event_content = current_user.event_contents.build
   end
@@ -15,7 +16,6 @@ class EventContentsController < ApplicationController
   def create
     @event_content = current_user.event_contents.build(event_content_params)
     @event_content.files = params[:event_content][:files]
-
     if @event_content.save
       redirect_to @event_content, notice: "Conteúdo registrado com sucesso."
     else
@@ -24,18 +24,15 @@ class EventContentsController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
-    @event_content = set_event_content
-    @event_content.files.attach(params[:event_content][:files])
-
+    @event_content.files = params[:event_content][:files]
     if @event_content.update(event_content_params)
       redirect_to @event_content, notice: "Conteúdo atualizado com sucesso!"
     else
       flash.now[:alert] = "Não foi possível atualizar seu Conteúdo."
-      render :new, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -51,5 +48,9 @@ class EventContentsController < ApplicationController
     rescue
       redirect_to events_path, notice: "Conteúdo Indisponível!"
     end
+  end
+
+  def set_event_content_files
+    @files = @event_content.files.select { |file| file.persisted? }
   end
 end
