b962e17c8a0eff74715adb8d2c812ee504e843d5
Pedro Dias <pedroddias98@gmail.com>
2025-01-31 16:09:16 -0300

    feat: Adicionando tela de visualização de tarefas cadastradas para uma programação
    Co-authored-by: Matheus Santana <matheusfsantana99@gmail.com>

---
+++ spec/system/curriculum_task/user_view_curriculum_task_details_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+describe 'User view curriculum task details', type: :system do
+  it 'with success' do
+    user = create(:user)
+    event =  [ build(:event, name: 'Ruby on Rails', description: 'Introdução ao Rails com TDD',
+                  start_date: 7.days.from_now, end_date: 14.days.from_now, url: 'www.meuevento.com/eventos/Ruby-on-Rails',
+                  event_type: 'Presencial', location: 'Juiz de Fora', participant_limit: 100, status: 'Publicado') ]
+    schedule_items = [ build(:schedule_item, id: 99, title: 'TDD com Rails', description: 'Introdução a programação com TDD') ]
+    curriculum = create(:curriculum, user: user, schedule_item_code: schedule_items.first.id)
+    create(:curriculum_task, curriculum: curriculum, title: 'Exercício Rails', description: 'Seu primeiro exercício', certificate_requirement: :optional)
+
+    allow(Event).to receive(:all).and_return(event)
+    allow(Event).to receive(:find).and_return(event.first)
+    allow(event.first).to receive(:schedule_items).and_return(schedule_items)
+    allow(ScheduleItem).to receive(:find).and_return(schedule_items.first)
+
+    login_as user, scope: :user
+    visit root_path
+    click_on 'Ruby on Rails'
+    click_on 'TDD com Rails'
+    click_on 'Exercício Rails'
+
+    expect(page).to have_content 'Exercício Rails'
+    expect(page).to have_content 'Seu primeiro exercício'
+    expect(page).to have_content 'Opcional'
+  end
+
+  it 'and must be authenticated' do
+    user = create(:user)
+    schedule_item = build(:schedule_item, id: 99, title: 'TDD com Rails', description: 'Introdução a programação com TDD')
+    curriculum = create(:curriculum, user: user, schedule_item_code: schedule_item.id)
+    task = create(:curriculum_task, curriculum: curriculum, title: 'Exercício Rails', description: 'Seu primeiro exercício', certificate_requirement: :optional)
+
+    visit curriculum_curriculum_task_path(curriculum, task)
+
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content 'Para continuar, faça login ou registre-se'
+  end
+
+  it 'and must be the curriculum task owner' do
+    user = create(:user)
+    second_user = create(:user)
+    schedule_item = build(:schedule_item, id: 99, title: 'TDD com Rails', description: 'Introdução a programação com TDD')
+    curriculum = create(:curriculum, user: user, schedule_item_code: schedule_item.id)
+    task = create(:curriculum_task, curriculum: curriculum, title: 'Exercício Rails', description: 'Seu primeiro exercício', certificate_requirement: :optional)
+
+    login_as second_user
+    visit curriculum_curriculum_task_path(curriculum, task)
+
+    expect(current_path).to eq events_path
+    expect(page).to have_content 'Conteúdo indisponível!'
+  end
+
+  it 'and go back to previous page' do
+    user = create(:user)
+    schedule_item = build(:schedule_item, id: 99, title: 'TDD com Rails', description: 'Introdução a programação com TDD')
+    curriculum = create(:curriculum, user: user, schedule_item_code: schedule_item.id)
+    task = create(:curriculum_task, curriculum: curriculum, title: 'Exercício Rails', description: 'Seu primeiro exercício', certificate_requirement: :optional)
+    allow(ScheduleItem).to receive(:find).and_return(schedule_item)
+
+    login_as user
+    visit curriculum_curriculum_task_path(curriculum, task)
+    click_on 'VOLTAR'
+
+    expect(current_path).to eq schedule_item_path(schedule_item.id)
+  end
+end
