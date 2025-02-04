0a84d8900a17b83e0cb9282a8fb0d0de57c174e5
Thiago Gois <thiagogoes1011@gmail.com>
2025-02-04 17:37:38 -0300

    feat: Processo de alteração do code de conteúdo e tarefa
    Co-authored-by: Pedro Dias <pedroddias98@gmail.com>

--- app/controllers/api/v1/curriculums_controller.rb
+++ app/controllers/api/v1/curriculums_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CurriculumsController < Api::V1::ApiController
   def show
-    @curriculum = Curriculum.find_by(schedule_item_code: params[:schedule_item_id])
+    @curriculum = Curriculum.find_by(schedule_item_code: params[:schedule_item_code])
 
     render status: :not_found, json: { error: I18n.t('not_found_error', model: Curriculum.model_name.human) } if @curriculum.nil?
   end
