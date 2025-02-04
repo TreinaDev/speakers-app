4e0bd5ae370eb210be707a6670f15068a6850526
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2025-02-04 16:39:59 -0300

    fix/111 refatoracao api envio de email e recebimento de codigo unico
    Co-authored-by: Bruno Herculano <herculanoterto@gmail.com>

--- app/services/external_event_api/user_find_email_service.rb
+++ app/services/external_event_api/user_find_email_service.rb
@@ -8,12 +8,7 @@ class ExternalEventApi::UserFindEmailService < ApplicationService
   def presence_fetch_api_email?
     result = nil
     begin
-      connection = Faraday.new do |conn|
-        conn.adapter Faraday.default_adapter
-        conn.headers['Content-Type'] = 'application/json'
-      end
-      email = { email: kwargs[:email] }
-      response = connection.post('http://localhost:3001/api/v1/speakers', email.to_json)
+      response = EventClient.post_auth_speaker_email_and_return_code(kwargs[:email])
       result = JSON.parse(response.body)
     rescue StandardError => error
       Rails.logger.error(error)
