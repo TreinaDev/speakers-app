83d2ef137f260bff41b8e6e910f3e9544c589232
Pedro Dias <pedroddias98@gmail.com>
2025-02-04 19:04:34 -0300

    Endpoint de currículo agora envia a url do vídeo externo do conteúdo pronta para ser renderizada. Adicionando métodos em ApiController para resgatar erros genéricos
    Co-authored-by: Thiago Gois <thiagogoes1011@gmail.com>
    Co-authored-by: Bruno Herculano <herculanoterto@gmail.com>

--- app/views/api/v1/curriculums/show.json.jbuilder
+++ app/views/api/v1/curriculums/show.json.jbuilder
@@ -5,7 +5,7 @@ if @curriculum.present?
         json.code content.event_content.code
         json.title content.event_content.title
         json.description content.event_content.description.body
-        json.external_video_url content.event_content.external_video_url
+        json.external_video_url render_external_video(content.event_content.external_video_url)
         if content.event_content.files.attached?
           json.files content.event_content.files do |file|
             json.filename file.filename
@@ -16,6 +16,7 @@ if @curriculum.present?
     end
     if  @curriculum.curriculum_tasks.any?
       json.curriculum_tasks @curriculum.curriculum_tasks do |task|
+        json.code task.code
         json.title task.title
         json.description task.description
         json.certificate_requirement task.translated_certificate_requirement(task.certificate_requirement)
--- spec/requests/api/v1/curriculum_api_spec.rb
+++ spec/requests/api/v1/curriculum_api_spec.rb
@@ -10,26 +10,28 @@ describe 'Curriculum API' do
       files = [ fixture_file_upload(Rails.root.join('spec/fixtures/capi.png')),
                 fixture_file_upload(Rails.root.join('spec/fixtures/nota-ufjf.pdf')),
                 fixture_file_upload(Rails.root.join('spec/fixtures/joker.mp4')) ]
-      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('12345678')
-      first_content = user.event_contents.create(title: 'Ruby PDF', description: '<strong>Descrição Ruby PDF</strong>',
-                                                 external_video_url: 'https://www.youtube.com/watch?v=idaXF2Er4TU')
+      first_content = user.event_contents.create(title: 'Ruby PDF', description: '<strong>Descrição Ruby PDF</strong>', code: 'ABCD1234',
+                                                 external_video_url: 'https://www.youtube.com/watch?v=idaXF2Er4TU', files: files)
       allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('ABCDEFGH')
       second_content = user.event_contents.create(title: 'Ruby Video', description: 'Apresentação sobre TDD',
                                                  external_video_url: 'https://www.youtube.com/watch?v=2DvrRadXwWY')
       allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('98764512')
       third_content = user.event_contents.create(title: 'Stimulus', description: 'PDF sobre Stimulus',
                                                  external_video_url: 'https://www.youtube.com/watch?v=1cw6qO1EYGw')
+      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('XLR8BE10')
       first_curriculum_content = create(:curriculum_content, id: 1, curriculum: curriculum, event_content: first_content)
+      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('CODIGO15')
       second_curriculum_content = create(:curriculum_content, id: 2, curriculum: curriculum, event_content: second_content)
+      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('CODIGO26')
       create(:curriculum_content, curriculum: curriculum, event_content: third_content)
+      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('CODIGO37')
       first_task = create(:curriculum_task, curriculum: curriculum, title: 'Exercício Rails', description: 'Seu primeiro exercício ruby', certificate_requirement: :mandatory)
+      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('CODIGO48')
       create(:curriculum_task, curriculum: curriculum, title: 'Exercício Stimulus', description: 'Seu primeiro exercício stimulus', certificate_requirement: :optional)
       create(:curriculum_task_content, curriculum_task: first_task, curriculum_content: first_curriculum_content)
       create(:curriculum_task_content, curriculum_task: first_task, curriculum_content: second_curriculum_content)
 
       allow(ScheduleItem).to receive(:find).and_return(schedule_item)
-
-
       get "/api/v1/curriculums/#{schedule_item.code}"
 
       expect(response).to have_http_status :success
@@ -38,26 +40,28 @@ describe 'Curriculum API' do
       tasks_response = curriculum_response['curriculum_tasks']
       contents_response = curriculum_response['curriculum_contents']
       expect(contents_response.length).to eq 3
-      expect(contents_response[0]['code']).to eq '12345678'
+      expect(contents_response[0]['code']).to eq first_content.code
       expect(contents_response[0]['title']).to eq 'Ruby PDF'
       expect(contents_response[0]['description']).to eq '<strong>Descrição Ruby PDF</strong>'
-      expect(contents_response[0]['external_video_url']).to eq 'https://www.youtube.com/watch?v=idaXF2Er4TU'
+      expect(contents_response[0]['external_video_url']).to eq "<iframe id='external-video' width='800' height='450' src='https://www.youtube.com/embed/idaXF2Er4TU' frameborder='0' allowfullscreen></iframe>"
       expect(contents_response[0]['files'][0]['filename']).to eq 'capi.png'
       expect(contents_response[0]['files'][1]['filename']).to eq 'nota-ufjf.pdf'
       expect(contents_response[0]['files'][2]['filename']).to eq 'joker.mp4'
-      expect(contents_response[1]['code']).to eq 'second_content.code'
+      expect(contents_response[1]['code']).to eq 'ABCDEFGH'
       expect(contents_response[1]['title']).to eq 'Ruby Video'
       expect(contents_response[1]['description']).to eq 'Apresentação sobre TDD'
-      expect(contents_response[1]['external_video_url']).to eq 'https://www.youtube.com/watch?v=2DvrRadXwWY'
+      expect(contents_response[1]['external_video_url']).to eq "<iframe id='external-video' width='800' height='450' src='https://www.youtube.com/embed/2DvrRadXwWY' frameborder='0' allowfullscreen></iframe>"
       expect(contents_response[2]['title']).to eq 'Stimulus'
       expect(contents_response[2]['description']).to eq 'PDF sobre Stimulus'
-      expect(contents_response[2]['external_video_url']).to eq 'https://www.youtube.com/watch?v=1cw6qO1EYGw'
+      expect(contents_response[2]['external_video_url']).to eq "<iframe id='external-video' width='800' height='450' src='https://www.youtube.com/embed/1cw6qO1EYGw' frameborder='0' allowfullscreen></iframe>"
       expect(tasks_response.length).to eq 2
+      expect(tasks_response[0]['code']).to eq 'CODIGO37'
       expect(tasks_response[0]['title']).to eq 'Exercício Rails'
       expect(tasks_response[0]['description']).to eq 'Seu primeiro exercício ruby'
       expect(tasks_response[0]['certificate_requirement']).to eq 'Obrigatória'
       expect(tasks_response[0]['attached_contents'][0]['attached_content_code']).to eq first_content.code
-      expect(tasks_response[0]['attached_contents'][1]['attached_content_code']).to eq second_content.code
+      expect(tasks_response[0]['attached_contents'][1]['attached_content_code']).to eq 'ABCDEFGH'
+      expect(tasks_response[1]['code']).to eq 'CODIGO48'
       expect(tasks_response[1]['title']).to eq 'Exercício Stimulus'
       expect(tasks_response[1]['description']).to eq 'Seu primeiro exercício stimulus'
       expect(tasks_response[1]['certificate_requirement']).to eq 'Opcional'
@@ -69,6 +73,21 @@ describe 'Curriculum API' do
       expect(response).to have_http_status :not_found
       expect(response.content_type).to include('application/json')
       expect(response.parsed_body['error']).to eq 'Currículo não encontrado!'
+    end
+
+    it 'internal server error' do
+      user = create(:user)
+      build(:event, name: 'Ruby on Rails')
+      schedule_item = build(:schedule_item, code: 99, name: 'TDD com Rails', description: 'Introdução a programação com TDD')
+      create(:curriculum, user: user, schedule_item_code: schedule_item.code)
+      user.event_contents.create(title: 'Ruby PDF')
+
+      allow(Curriculum).to receive(:find_by).and_raise(ActiveRecord::ActiveRecordError)
+      get "/api/v1/curriculums/#{schedule_item.code}"
+
+      expect(response).to have_http_status 500
+      expect(response.content_type).to include('application/json')
+      expect(response.parsed_body['error']).to eq 'Algo deu errado.'
     end
   end
 end
