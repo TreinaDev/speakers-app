03829f986d8d084327928fe3ab54f4004f87c3d9
Bruno-H-Terto <herculanoterto@gmail.com>
2025-02-05 15:12:07 -0300

    fix: Adicionado testes de paginação

---
+++ spec/system/paginators/index_event_pagination_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+describe 'Index Event pagination', js: :true do
+  it 'should advance the page when clicking next' do
+    Event.delete_all
+    events = build_list(:event, 16)
+    allow(Event).to receive(:all).and_return(events)
+    user = create(:user)
+    create(:profile, user: user)
+
+    login_as user, scope: :user
+    visit events_path
+    expect(page).to have_selector('.event__card', count: 15)
+    click_on 'Próxima'
+    expect(page).to have_selector('.event__card', count: 1)
+  end
+
+  it 'should advance the last page when clicking last' do
+    Event.delete_all
+    events = build_list(:event, 31)
+    allow(Event).to receive(:all).and_return(events)
+    user = create(:user)
+    create(:profile, user: user)
+
+    login_as user, scope: :user
+    visit events_path
+    expect(page).to have_selector('.event__card', count: 15)
+    click_on 'Última'
+    expect(page).to have_selector('.event__card', count: 1)
+  end
+
+  it 'should return a page when clicking previous' do
+    Event.delete_all
+    events = build_list(:event, 16)
+    allow(Event).to receive(:all).and_return(events)
+    user = create(:user)
+    create(:profile, user: user)
+
+    login_as user, scope: :user
+    visit events_path
+    expect(page).to have_selector('.event__card', count: 15)
+    click_on 'Próxima'
+    expect(page).to have_selector('.event__card', count: 1)
+    click_on 'Anterior'
+    expect(page).to have_selector('.event__card', count: 15)
+  end
+
+  it 'should advance the first page when clicking first' do
+    Event.delete_all
+    events = build_list(:event, 31)
+    allow(Event).to receive(:all).and_return(events)
+    user = create(:user)
+    create(:profile, user: user)
+
+    login_as user, scope: :user
+    visit events_path
+    expect(page).to have_selector('.event__card', count: 15)
+    click_on 'Última'
+    expect(page).to have_selector('.event__card', count: 1)
+    click_on 'Primeira'
+    expect(page).to have_selector('.event__card', count: 15)
+  end
+
+  it 'should advance the number page when clicking number' do
+    Event.delete_all
+    events = build_list(:event, 31)
+    allow(Event).to receive(:all).and_return(events)
+    user = create(:user)
+    create(:profile, user: user)
+
+    login_as user, scope: :user
+    visit events_path
+    expect(page).to have_selector('.event__card', count: 15)
+    find('#page_2 a', text: '2').click
+    expect(page).to have_selector('.event__card', count: 15)
+    find('#page_3 a', text: '3').click
+    expect(page).to have_selector('.event__card', count: 1)
+  end
+end
