242e7b908ebe937040988ac74e80fd20221fa77e
Bruno-H-Terto <herculanoterto@gmail.com>
2025-02-08 03:42:18 -0300

    feat: Ajustes em collapse e modal de imagens expandidas

--- app/javascript/controllers/collapse_controller.js
+++ app/javascript/controllers/collapse_controller.js
@@ -6,11 +6,19 @@ export default class extends Controller {
 
   connect() {
     this.expanded = false
+    this.buttonTarget.classList.add("expanded")
   }
 
   toggle() {
     this.expanded = !this.expanded
     this.contentTarget.classList.toggle("hidden")
     this.buttonTarget.textContent = this.expanded ? "Mostrar menos" : "Mostrar mais"
+    if (this.expanded) {
+      this.buttonTarget.classList.add("contract")
+      this.buttonTarget.classList.remove("expanded")
+    } else {
+      this.buttonTarget.classList.add("expanded")
+      this.buttonTarget.classList.remove("contract")
+    }
   }
 }
