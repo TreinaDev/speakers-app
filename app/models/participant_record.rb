a25c2f0127636974bf18af9599fb800603973e13
Thiago Gois <thiagogoes1011@gmail.com>
2025-02-08 16:56:11 -0300

    feat: Lógica para atualização do enabled_certificate implementado

--- app/models/participant_record.rb
+++ app/models/participant_record.rb
@@ -3,4 +3,15 @@ class ParticipantRecord < ApplicationRecord
   has_many :participant_tasks
 
   validates :schedule_item_code, :participant_code, presence: true
+
+  def change_enabled_certificate(curriculum, new_task)
+    if new_task.curriculum_task.mandatory?
+      curriculum_mandatory_task_codes = curriculum.curriculum_tasks.mandatory.map { |task| task.code }
+
+      participant_mandatory_task_codes = participant_tasks.map { |task| task.curriculum_task.code if task.curriculum_task.mandatory? }.compact
+      participant_mandatory_task_codes << new_task.curriculum_task.code
+      update(enabled_certificate: true) if curriculum_mandatory_task_codes.length == participant_mandatory_task_codes.length &&
+                                        curriculum_mandatory_task_codes.all? { |task| participant_mandatory_task_codes.include?(task) }
+    end
+  end
 end
--- spec/requests/api/v1/participant_task_api_spec.rb
+++ spec/requests/api/v1/participant_task_api_spec.rb
@@ -18,7 +18,6 @@ describe 'POST /api/v1/participant_tasks' do
     expect(ParticipantRecord.count).to eq 1
     expect(ParticipantRecord.first.attributes.symbolize_keys).to match(a_hash_including(user_id: 10, participant_code: 'XLR8BEN1',
                                                                                         schedule_item_code: curriculum.schedule_item_code, enabled_certificate: false))
-
     expect(ParticipantTask.count).to eq 1
     expect(ParticipantTask.first.attributes.symbolize_keys).to match(a_hash_including(participant_record_id: ParticipantRecord.first.id, curriculum_task_id: task.id,
                                                                                       task_status: true))
@@ -71,5 +70,30 @@ describe 'POST /api/v1/participant_tasks' do
     expect(response).to have_http_status :internal_server_error
     expect(response.content_type).to include('application/json')
     expect(response.parsed_body['error']).to eq 'Algo deu errado.'
+  end
+
+  it 'with the last mandatory task' do
+    user = create(:user)
+    schedule_item = build(:schedule_item, code: 'ABCD1234', name: 'TDD com Rails', description: 'Introdução a programação com TDD')
+    curriculum = create(:curriculum, user: user, schedule_item_code: schedule_item.code)
+    task = create(:curriculum_task, curriculum: curriculum, title: 'Exercício Rails', code: '1234ABCD',
+                   description: 'Seu primeiro exercício ruby', certificate_requirement: :mandatory)
+    task_2 = create(:curriculum_task, curriculum: curriculum, title: 'Exercício JavaScript', code: '4785ZRCD',
+                   description: 'Seu primeiro exercício JavaScript', certificate_requirement: :optional)
+    task_3 = create(:curriculum_task, curriculum: curriculum, title: 'Exercício Ruby', code: '1234ZRCD',
+                   description: 'Seu primeiro exercício ruby puro', certificate_requirement: :mandatory)
+    record = create(:participant_record, participant_code: 'XLR8BEN1', schedule_item_code: schedule_item.code, user: user)
+    create(:participant_task, participant_record: record, curriculum_task: task, task_status: true)
+    create(:participant_task, participant_record: record, curriculum_task: task_2, task_status: true)
+
+    allow(ScheduleItem).to receive(:find).and_return(schedule_item)
+    post '/api/v1/participant_tasks', params: { participant_code: 'XLR8BEN1', task_code: task_3.code }
+
+    expect(response).to have_http_status :success
+    expect(response.content_type).to include('application/json')
+    json_response = JSON.parse(response.body)
+    expect(json_response['message']).to eq('OK')
+    expect(ParticipantRecord.count).to eq 1
+    expect(ParticipantRecord.first.enabled_certificate).to eq(true)
   end
 end
