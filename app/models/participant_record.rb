e450cd833cdcc4aef0cf7b7b45b263fefebe5f5e
Thiago Gois <thiagogoes1011@gmail.com>
2025-02-08 18:54:03 -0300

    testes de modelo adicionados

--- app/models/participant_record.rb
+++ app/models/participant_record.rb
@@ -5,7 +5,7 @@ class ParticipantRecord < ApplicationRecord
   validates :schedule_item_code, :participant_code, presence: true
 
   def change_enabled_certificate(curriculum, new_task)
-    if new_task.curriculum_task.mandatory?
+    if new_task.present? && new_task.curriculum_task.mandatory? && curriculum.present?
       curriculum_mandatory_task_codes = curriculum.curriculum_tasks.mandatory.map { |task| task.code }
 
       participant_mandatory_task_codes = participant_tasks.map { |task| task.curriculum_task.code if task.curriculum_task.mandatory? }.compact
--- spec/models/participant_record_spec.rb
+++ spec/models/participant_record_spec.rb
@@ -7,4 +7,30 @@ RSpec.describe ParticipantRecord, type: :model do
     it { should validate_presence_of(:participant_code) }
     it { should validate_presence_of(:schedule_item_code) }
   end
+
+  context '.change_enabled_certificate' do
+    it 'with success' do
+      user = create(:user)
+      curriculum = create(:curriculum, user: user)
+      task = create(:curriculum_task, curriculum: curriculum, title: 'Exercício Rails',
+                   description: 'Seu primeiro exercício ruby', certificate_requirement: :mandatory)
+      record = create(:participant_record, user: user, enabled_certificate: false)
+      participant_task = build(:participant_task, participant_record: record, curriculum_task: task, task_status: true)
+
+      record.change_enabled_certificate(curriculum, participant_task)
+
+      expect(record.enabled_certificate).to eq(true)
+    end
+
+    it 'with curriculum and participant_task nil' do
+      user = create(:user)
+      curriculum = nil
+      record = create(:participant_record, user: user, enabled_certificate: false)
+      participant_task = nil
+
+      record.change_enabled_certificate(curriculum, participant_task)
+
+      expect(record.enabled_certificate).to eq(false)
+    end
+  end
 end
